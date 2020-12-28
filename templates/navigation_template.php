3519b1db65edcc520d000d28a83cde1393c48300
Jimmi08 <janamicek@gmail.com>
2020-12-28 12:23:58 +0100

    changes related to navigation

--- templates/navigation_template.php
+++ templates/navigation_template.php
@@ -8,6 +8,7 @@
 
 /* it should be templated this way:
 - main........horizontal with dropdown   	and ul/li/a version, ul.navbar-nav + li.nav-item + a.nav-link
+- main-alt....horizontal with dropdown   	and div/a version, div.navbar-nav + a.nav-link
 - side........vertical   with list-group 	and ul/li/a version, ul.list-group + li.list-group-item + a
 - side-alt....vertical   with list-group 	and div/a version, 	 div.list-group + a.list-group-item.list-group-item-action 
 - footer......horizontal with dropdown   	and ul/li/a version, ul.nav + li.nav-item + a.nav-link.
@@ -17,40 +18,45 @@
 */
  
 // TEMPLATE FOR {NAVIGATION=main}
-$NAVIGATION_TEMPLATE['main']['start'] = '<ul class="navbar-nav">';    
+$element_class           = ' class="navbar-nav {NAV_CLASS}" ';
+$item_class              = ' class="nav-item" ';
+$item_class_active       = ' class="nav-item active" ';
+$item_class_submenu      = ' class="nav-item dropdown {NAV_IDENTIFIER}" ';
+
+$NAVIGATION_TEMPLATE['main']['start'] = "<ul {$element_class} >";    
 
 // Main Link
-$NAVIGATION_TEMPLATE['main']['item'] = '<li class="nav-item"><a  class="nav-link"  role="button" href="{LINK_URL}"{LINK_OPEN} title="{LINK_DESCRIPTION}">{LINK_ICON}{LINK_NAME}</a></li>';
+$NAVIGATION_TEMPLATE['main']['item'] = "<li {$item_class}><a  class='nav-link'  role='button' href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a></li>";
 
 // Main Link - active state
-$NAVIGATION_TEMPLATE['main']['item_active'] = '<li class="nav-item active"><a class="nav-link e-tip" role="button" data-target="#" href="{LINK_URL}"{LINK_OPEN} title="{LINK_DESCRIPTION}">{LINK_ICON} {LINK_NAME}</a></li>';
+$NAVIGATION_TEMPLATE['main']['item_active'] = "<li {$item_class_active}><a  class='nav-link'  role='button' href='{NAV_URL}' title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a></li>";
 
 $NAVIGATION_TEMPLATE['main']['end'] = '</ul>';
 
 // Main Link which has a sub menu. 
-$NAVIGATION_TEMPLATE['main']['item_submenu'] = '
-	<li class="nav-item dropdown {LINK_IDENTIFIER}">
-		<a class="nav-link dropdown-toggle"  role="button" data-toggle="dropdown" id="navbarDropdownMenuLink-{LINK_ID}" data-target="#" aria-haspopup="true" aria-expanded="false" href="{LINK_URL}" title="{LINK_DESCRIPTION}">
-		 {LINK_ICON}{LINK_NAME} 
+$NAVIGATION_TEMPLATE['main']['item_submenu'] = "
+	<li {$item_class_active}>
+		<a class='nav-link dropdown-toggle'  role='button' data-toggle='dropdown' id='navbarDropdownMenuLink-{NAV_ID}' data-target='#' aria-haspopup='true' aria-expanded='false' href='{NAV_URL}' title='{NAV_DESCRIPTION}'>
+		 {NAV_ICON}{NAV_NAME} 
 		</a> 
-		{LINK_SUB}
+		{NAV_SUB}
 	</li>
-';
+	";
 
 // Main Link which has a sub menu - active state.
 $NAVIGATION_TEMPLATE['main']['item_submenu_active'] = '
-	<li class="nav-item dropdown active {LINK_IDENTIFIER}">
-		<a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" id="navbarDropdownMenuLink-{LINK_ID}" aria-haspopup="true" aria-expanded="true" data-target="#" href="{LINK_URL}">
-		 {LINK_ICON}{LINK_NAME}
+	<li class="nav-item dropdown active {NAV_IDENTIFIER}">
+		<a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" id="navbarDropdownMenuLink-{NAV_ID}" aria-haspopup="true" aria-expanded="true" data-target="#" href="{NAV_URL}">
+		 {NAV_ICON}{NAV_NAME}
 		</a>
-		{LINK_SUB}
+		{NAV_SUB}
 	</li>
 ';	
 
-// Sub menu BUG:  aria-labelledby= - it should be the same as navbarDropdownMenuLink-{LINK_ID} from submenu parent but LINK_PARENT returns 0  
-$NAVIGATION_TEMPLATE['main']['submenu_start'] = '<div class="dropdown-menu submenu-start submenu-level-{LINK_DEPTH}" aria-labelledby="navbarDropdownMenuLink-{LINK_PARENT}">';
-$NAVIGATION_TEMPLATE['main']['submenu_item'] = '<a class="dropdown-item" href="{LINK_URL}"{LINK_OPEN}>{LINK_ICON}{LINK_NAME}</a>';
-$NAVIGATION_TEMPLATE['main']['submenu_item_active'] = '<a class="dropdown-item active" href="{LINK_URL}"{LINK_OPEN}>{LINK_ICON}{LINK_NAME}</a> ';
+// Sub menu BUG:  aria-labelledby= - it should be the same as navbarDropdownMenuLink-{NAV_ID} from submenu parent but LINK_PARENT returns 0  
+$NAVIGATION_TEMPLATE['main']['submenu_start'] = '<div class="dropdown-menu submenu-start submenu-level-{NAV_DEPTH}" aria-labelledby="navbarDropdownMenuLink-{NAV_PARENT}">';
+$NAVIGATION_TEMPLATE['main']['submenu_item'] = '<a class="dropdown-item" href="{NAV_URL}"{NAV_OPEN}>{NAV_ICON}{NAV_NAME}</a>';
+$NAVIGATION_TEMPLATE['main']['submenu_item_active'] = '<a class="dropdown-item active" href="{NAV_URL}"{NAV_OPEN}>{NAV_ICON}{NAV_NAME}</a> ';
 $NAVIGATION_TEMPLATE['main']['submenu_end'] = '</div>';
 
 // 3rd Sub menu
@@ -58,6 +64,24 @@ $NAVIGATION_TEMPLATE['main']['submenu_lowerstart'] = '';
 $NAVIGATION_TEMPLATE['main']['submenu_loweritem'] = '';
 $NAVIGATION_TEMPLATE['main']['submenu_loweritem_active'] = '';
 $NAVIGATION_TEMPLATE['main']['submenu_lowerend'] = '';
+
+
+/* 
+ ALTERNATIVE MARKUP for main navigation:
+ {NAVIGATION: layout=main-alt&type=any} 
+ div/a version with dropdown, 2 levels
+ see https://getbootstrap.com/docs/4.5/components/navbar/#nav
+*/
+
+$NAVIGATION_TEMPLATE['main_alt'] = $NAVIGATION_TEMPLATE['main'];
+$element_class           = ' class="navbar-nav {NAV_CLASS}" ';
+$item_class              = ' class="nav-link" ';
+$item_class_active       = ' class="nav-link active" ';
+
+$NAVIGATION_TEMPLATE['main-alt']['start'] = "<div {$element_class}>";   
+$NAVIGATION_TEMPLATE['main-alt']['item'] = "<a {$item_class} href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>";
+$NAVIGATION_TEMPLATE['main-alt']['item_active'] = "<a {$item_class_active} href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>"; 
+$NAVIGATION_TEMPLATE['main-alt']['end'] = '</div>';
 
 /* 
  DEFAULT LIST-GROUP SIDE TEMPLATE FOR:
@@ -67,22 +91,22 @@ $NAVIGATION_TEMPLATE['main']['submenu_lowerend'] = '';
  see https://getbootstrap.com/docs/4.1/components/list-group/#basic-example
 */ 
  
-$element_class       = ' class="list-group" ';
+$element_class       = ' class="list-group {NAV_CLASS}" ';
 $item_class          = ' class="list-group-item" ';
 $item_class_active   = ' class="list-group-item active" ';
 $item_class_submenu  = ' class="list-group-item disabled" ';
 
 $NAVIGATION_TEMPLATE['side']['start'] 				= "<ul {$element_class}>";
-$NAVIGATION_TEMPLATE['side']['item'] 				= "<li {$item_class}><a href='{LINK_URL}'{LINK_OPEN} title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a></li>";
-$NAVIGATION_TEMPLATE['side']['item_active'] 		= "<li {$item_class_active}><a href='{LINK_URL}' title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a></li>";
+$NAVIGATION_TEMPLATE['side']['item'] 				= "<li {$item_class}><a href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a></li>";
+$NAVIGATION_TEMPLATE['side']['item_active'] 		= "<li {$item_class_active}><a href='{NAV_URL}' title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a></li>";
 $NAVIGATION_TEMPLATE['side']['end'] 				= '</ul>';
 
 // 2rd Sub menu
-$NAVIGATION_TEMPLATE['side']['item_submenu'] 		= "<li {$item_class_submenu}>{LINK_ICON}{LINK_NAME}{LINK_SUB}</li>";
-$NAVIGATION_TEMPLATE['side']['item_submenu_active'] = "<li {$item_class_submenu_active}>{LINK_ICON}{LINK_NAME}{LINK_SUB}</li>"; 				
+$NAVIGATION_TEMPLATE['side']['item_submenu'] 		= "<li {$item_class_submenu}>{NAV_ICON}{NAV_NAME}{NAV_SUB}</li>";
+$NAVIGATION_TEMPLATE['side']['item_submenu_active'] = "<li {$item_class_submenu_active}>{NAV_ICON}{NAV_NAME}{NAV_SUB}</li>"; 				
 $NAVIGATION_TEMPLATE['side']['submenu_start'] 		= "";
-$NAVIGATION_TEMPLATE['side']['submenu_item']		= "<li {$item_class}><a href='{LINK_URL}' {LINK_OPEN}>{LINK_ICON}{LINK_NAME}</a></li>";
-$NAVIGATION_TEMPLATE['side']['submenu_item_active']	= "<li {$item_class_active}><a href='{LINK_URL}'>{LINK_ICON}{LINK_NAME}</a></li>"; 
+$NAVIGATION_TEMPLATE['side']['submenu_item']		= "<li {$item_class}><a href='{NAV_URL}' {NAV_OPEN}>{NAV_ICON}{NAV_NAME}</a></li>";
+$NAVIGATION_TEMPLATE['side']['submenu_item_active']	= "<li {$item_class_active}><a href='{NAV_URL}'>{NAV_ICON}{NAV_NAME}</a></li>"; 
 // 3rd Sub menu
 $NAVIGATION_TEMPLATE['side']['submenu_lowerstart'] 	     = "";
 $NAVIGATION_TEMPLATE['side']['submenu_loweritem'] 		 = "";
@@ -98,20 +122,20 @@ $NAVIGATION_TEMPLATE['side']['submenu_lowerend'] 		 = "";
 
 $NAVIGATION_TEMPLATE['side-alt'] = $NAVIGATION_TEMPLATE['side'];
 
-$element_class           = ' class="list-group" ';
+$element_class           = ' class="list-group {NAV_CLASS}" ';
 $item_class              = ' class="list-group-item list-group-item-action" ';
 $item_class_active       = ' class="list-group-item list-group-item-action active" ';
 $item_class_submenu      = ' class="list-group-item list-group-item-action disabled" ';
  
 $NAVIGATION_TEMPLATE['side-alt']['start'] 				= "<div {$element_class}>";
-$NAVIGATION_TEMPLATE['side-alt']['item'] 				= "<a {$item_class} href='{LINK_URL}'{LINK_OPEN} title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a>";
+$NAVIGATION_TEMPLATE['side-alt']['item'] 				= "<a {$item_class} href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>";
 
-$NAVIGATION_TEMPLATE['side-alt']['item_active'] 		= "<a {$item_class_active} href='{LINK_URL}' title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a>";
+$NAVIGATION_TEMPLATE['side-alt']['item_active'] 		= "<a {$item_class_active} href='{NAV_URL}' title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>";
 $NAVIGATION_TEMPLATE['side-alt']['end'] 				= '</div>';
 
-$NAVIGATION_TEMPLATE['side-alt']['item_submenu'] 		= "<a {$item_class_submenu}> {LINK_ICON}{LINK_NAME}</a>{LINK_SUB}";
-$NAVIGATION_TEMPLATE['side-alt']['submenu_item']		= "<a {$item_class} href='{LINK_URL}' {LINK_OPEN}>{LINK_ICON}{LINK_NAME}</a>";
-$NAVIGATION_TEMPLATE['side-alt']['submenu_item_active']	= "<a {$item_class_active} href='{LINK_URL}' {LINK_OPEN}>{LINK_ICON}{LINK_NAME}</a>"; 
+$NAVIGATION_TEMPLATE['side-alt']['item_submenu'] 		= "<a {$item_class_submenu}> {NAV_ICON}{NAV_NAME}</a>{NAV_SUB}";
+$NAVIGATION_TEMPLATE['side-alt']['submenu_item']		= "<a {$item_class} href='{NAV_URL}' {NAV_OPEN}>{NAV_ICON}{NAV_NAME}</a>";
+$NAVIGATION_TEMPLATE['side-alt']['submenu_item_active']	= "<a {$item_class_active} href='{NAV_URL}' {NAV_OPEN}>{NAV_ICON}{NAV_NAME}</a>"; 
 
 /* 
  DEFAULT HORIZONTAL FOOTER TEMPLATE FOR:
@@ -121,7 +145,7 @@ $NAVIGATION_TEMPLATE['side-alt']['submenu_item_active']	= "<a {$item_class_activ
  see https://getbootstrap.com/docs/4.1/components/navs/#base-nav
 */ 
 
-$element_class       		= ' class="nav" ';
+$element_class       		= ' class="nav {NAV_CLASS}" ';
 $item_class          		= ' class="nav-item" ';
 $item_class_active   		= ' class="nav-item active" ';
 $item_class_submenu  		= ' class="nav-item dropdown" ';
@@ -129,28 +153,28 @@ $item_class_submenu_active  = ' class="nav-item dropdown active" ';
 
 $NAVIGATION_TEMPLATE['footer']['start'] 				= "<ul {$element_class}>";
 $NAVIGATION_TEMPLATE['footer']['item'] 					= "<li {$item_class}>
-    <a class='nav-link' href='{LINK_URL}'{LINK_OPEN} title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a></li>";
-$NAVIGATION_TEMPLATE['footer']['item_active'] 			= "<li {$item_class_active} {LINK_OPEN}>
-	<a class='nav-link' href='{LINK_URL}' title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a></li>";
+    <a class='nav-link' href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a></li>";
+$NAVIGATION_TEMPLATE['footer']['item_active'] 			= "<li {$item_class_active} {NAV_OPEN}>
+	<a class='nav-link' href='{NAV_URL}' title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a></li>";
 $NAVIGATION_TEMPLATE['footer']['end'] 					= '</ul>';
 
 // 2rd Sub menu   data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
 $NAVIGATION_TEMPLATE['footer']['item_submenu'] 			= "
 <li {$item_class_submenu}>
 	<a class='nav-link dropdown-toggle'  role='button' data-toggle='dropdown' data-target='#' aria-haspopup='true' 
-	       aria-expanded='false' href='#' title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a> 
-	{LINK_SUB}
+	       aria-expanded='false' href='#' title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a> 
+	{NAV_SUB}
 </li>";
 $NAVIGATION_TEMPLATE['footer']['item_submenu_active'] 	= "
 <li {$item_class_submenu_active}>
-	<a class='nav-link dropdown-toggle'  role='button' data-toggle='dropdown'data-target='#' aria-haspopup='true' aria-expanded='false' href='{LINK_URL}' title='{LINK_DESCRIPTION}'>
-	 {LINK_ICON}{LINK_NAME} 
+	<a class='nav-link dropdown-toggle'  role='button' data-toggle='dropdown'data-target='#' aria-haspopup='true' aria-expanded='false' href='{NAV_URL}' title='{NAV_DESCRIPTION}'>
+	 {NAV_ICON}{NAV_NAME} 
 	</a> 
-	{LINK_SUB}
+	{NAV_SUB}
 </li>";
 $NAVIGATION_TEMPLATE['footer']['submenu_start'] 		= "<div class='dropdown-menu'>";
-$NAVIGATION_TEMPLATE['footer']['submenu_item']			= "<a class='dropdown-item' href='{LINK_URL}'{LINK_OPEN} title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a>";
-$NAVIGATION_TEMPLATE['footer']['submenu_item_active'] 	= "<a class='dropdown-item active' href='{LINK_URL}'{LINK_OPEN} title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a>"; 
+$NAVIGATION_TEMPLATE['footer']['submenu_item']			= "<a class='dropdown-item' href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>";
+$NAVIGATION_TEMPLATE['footer']['submenu_item_active'] 	= "<a class='dropdown-item active' href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>"; 
 $NAVIGATION_TEMPLATE['footer']['submenu_end'] 			= "</div>";
 
 // 3rd Sub menu
@@ -168,14 +192,14 @@ $NAVIGATION_TEMPLATE['footer']['submenu_lowerend'] 		 	= "";
 
 $NAVIGATION_TEMPLATE['footer-alt'] = $NAVIGATION_TEMPLATE['footer'];
 
-$element_class           = ' class="nav" ';
+$element_class           = ' class="nav {NAV_CLASS}" ';
 $item_class              = ' class="nav-link" ';
 $item_class_active       = ' class="nav-link active" ';
 $item_class_submenu      = ' class="nav-link disabled" ';
 
 $NAVIGATION_TEMPLATE["footer-alt"]["start"] 				= "<nav {$element_class}>";
-$NAVIGATION_TEMPLATE["footer-alt"]["item"] 					= "<a {$item_class} href='{LINK_URL}'{LINK_OPEN} title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a>";
-$NAVIGATION_TEMPLATE["footer-alt"]["item_active"] 			= "<a {$item_class_active} href='{LINK_URL}' title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a>";
+$NAVIGATION_TEMPLATE["footer-alt"]["item"] 					= "<a {$item_class} href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>";
+$NAVIGATION_TEMPLATE["footer-alt"]["item_active"] 			= "<a {$item_class_active} href='{NAV_URL}' title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>";
 $NAVIGATION_TEMPLATE["footer-alt"]["end"] 					= "</nav>";
  
  
@@ -188,16 +212,16 @@ $NAVIGATION_TEMPLATE["footer-alt"]["end"] 					= "</nav>";
 
 $NAVIGATION_TEMPLATE['alt'] 	 = $NAVIGATION_TEMPLATE['footer'];
 
-$element_class           = ' class="nav flex-column" ';
+$element_class           = ' class="nav flex-column {NAV_CLASS}" ';
 $item_class              = ' class="nav-item" ';
 $item_class_active       = ' class="nav-item active" ';
 $item_class_submenu      = ' class="nav-item disabled" ';
 
 $NAVIGATION_TEMPLATE['alt']['start'] 		 = "<nav {$element_class}>";
 $NAVIGATION_TEMPLATE['alt']['item'] 		 = "<li {$item_class}>
-		<a class='nav-link' href='{LINK_URL}'{LINK_OPEN} title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a></li>";
-$NAVIGATION_TEMPLATE['alt']['item_active'] 	 = "<li {$item_class_active} {LINK_OPEN}>
-		<a class='nav-link' href='{LINK_URL}' title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a></li>";
+		<a class='nav-link' href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a></li>";
+$NAVIGATION_TEMPLATE['alt']['item_active'] 	 = "<li {$item_class_active} {NAV_OPEN}>
+		<a class='nav-link' href='{NAV_URL}' title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a></li>";
 $NAVIGATION_TEMPLATE['alt']['end'] 					= '</nav>';
  
  
@@ -211,17 +235,15 @@ $NAVIGATION_TEMPLATE['alt']['end'] 					= '</nav>';
 
 $NAVIGATION_TEMPLATE['alt5'] 	 = $NAVIGATION_TEMPLATE['footer'];
 
-$element_class           = ' class="nav" ';
+$element_class           = ' class="nav {NAV_CLASS}" ';
 $item_class              = ' class="nav-item nav-link" ';
 $item_class_active       = ' class="nav-item nav-link active" ';
 $item_class_submenu      = ' class="nav-link dropdown-toggle" ';
 
 $NAVIGATION_TEMPLATE["alt5"]["start"] 				= "<nav {$element_class}>";
-$NAVIGATION_TEMPLATE["alt5"]["item"] 					= "<a {$item_class} href='{LINK_URL}'{LINK_OPEN} title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a>";
-$NAVIGATION_TEMPLATE["alt5"]["item_active"] 			= "<a {$item_class_active} href='{LINK_URL}' title='{LINK_DESCRIPTION}'>{LINK_ICON}{LINK_NAME}</a>";
+$NAVIGATION_TEMPLATE["alt5"]["item"] 					= "<a {$item_class} href='{NAV_URL}'{NAV_OPEN} title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>";
+$NAVIGATION_TEMPLATE["alt5"]["item_active"] 			= "<a {$item_class_active} href='{NAV_URL}' title='{NAV_DESCRIPTION}'>{NAV_ICON}{NAV_NAME}</a>";
 $NAVIGATION_TEMPLATE["alt5"]["end"] 					= "</nav>";
-
-
 
 $NAVIGATION_TEMPLATE['alt6'] 	 = $NAVIGATION_TEMPLATE['alt5'];
 
